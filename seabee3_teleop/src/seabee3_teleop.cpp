7f4930cfd48dd005562633bd31ab2abc1a84b53c
Edward T. Kaszubski <ekaszubski@gmail.com>
2010-06-25 16:05:12 +0000

    Cleaned up seabee3_teleop

--- seabee3_teleop/src/seabee3_teleop.cpp
+++ seabee3_teleop/src/seabee3_teleop.cpp
@@ -3,30 +3,30 @@
 #include <joy/Joy.h>
 
 ros::Publisher * cmd_vel_pub;
-int speed, strafe, surface, dive, heading;
-double speed_s, strafe_s, surface_s, dive_s, heading_s;
+int speed, strafe, surface, dive, heading, roll;
+double speed_s, strafe_s, surface_s, dive_s, heading_s, roll_s;
+
+double applyDeadZone (double value)
+{
+	return fabs(value) < 0.15 ? 0.0 : value;
+}
 
 void joyCallback(const joy::Joy::ConstPtr& joy)
 {
-	ROS_INFO("joy_callback");
 	geometry_msgs::Twist cmd_vel;
 	
-	double joy_speed = joy->axes[speed];
-	joy_speed = abs(joy_speed) < 0.15 ? 0.0 : joy_speed;
-	double joy_strafe = joy->axes[strafe];
-	joy_strafe = abs(joy_strafe) < 0.15 ? 0.0 : joy_strafe;
-	double joy_dive = joy->axes[dive];
-	joy_dive = abs(joy_dive) < 0.15 ? 0.0 : joy_dive;
-	double joy_surface = joy->axes[surface];
-	joy_surface = abs(joy_surface) < 0.15 ? 0.0 : joy_surface;
-	double joy_heading = joy->axes[heading];
-	joy_heading = abs(joy_heading) < 0.15 ? 0.0 : joy_heading;
+	double joy_speed = 		applyDeadZone( (double)(joy->axes[speed]) );
+	double joy_strafe = 	applyDeadZone( (double)(joy->axes[strafe]) );
+	double joy_dive = 		applyDeadZone( (double)(joy->axes[dive]) );
+	double joy_surface = 	applyDeadZone( (double)(joy->axes[surface]) );
+	double joy_heading = 	applyDeadZone( (double)(joy->axes[heading]) );
+	double joy_roll = 		applyDeadZone( (double)(joy->axes[roll]) );
 	
 	cmd_vel.linear.x = speed_s * joy_speed; //speed
 	cmd_vel.linear.y = strafe_s * joy_strafe; //strafe
 	cmd_vel.linear.z = 0.5 * (float)(dive_s * joy_dive - surface_s * joy_surface); //dive - surface
 	
-	cmd_vel.angular.x = 0;
+	cmd_vel.angular.x = roll_s * joy_roll; //roll
 	cmd_vel.angular.y = 0;
 	cmd_vel.angular.z = heading_s * joy_heading; //heading
 	
@@ -43,12 +43,14 @@ int main(int argc, char** argv)
 	n.param("surface", surface, 2);
 	n.param("dive", dive, 3);
 	n.param("heading", heading, 4);
+	n.param("roll", roll, 5);
 	
 	n.param("speed_scale", speed_s, 1.0);
 	n.param("strafe_scale", strafe_s, 1.0);
 	n.param("surface_scale", surface_s, 1.0);
 	n.param("dive_scale", dive_s, 1.0);
 	n.param("heading_scale", heading_s, 1.0);
+	n.param("roll_scale", roll_s, 1.0);
 	
 	ros::Subscriber joy_sub = n.subscribe("joy", 1, joyCallback);
 	
