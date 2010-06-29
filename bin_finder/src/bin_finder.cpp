e151d2f97e964efeb5b51643dc0a7667839a9561
Michael Wei <SubWei08@gmail.com>
2010-06-29 02:13:12 +0000

    Added bin_finder

---
+++ bin_finder/src/bin_finder.cpp
@@ -0,0 +1,398 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/CvBridge.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+
+#define MIN_CENTER_DIST		15
+#define MIN_AREA					150
+#define CORNER_TOLERANCE	4
+
+int thresh = 42;
+double angle_thresh = 0.4;
+uint itsWidth = 320;
+uint itsHeight = 240;
+
+// helper function:
+// finds a cosine of angle between vectors
+// from pt0->pt1 and from pt0->pt2
+double angle(CvPoint* pt1, CvPoint* pt2, CvPoint* pt0)
+{
+	double dx1 = pt1->x - pt0->x;
+	double dy1 = pt1->y - pt0->y;
+	double dx2 = pt2->x - pt0->x;
+	double dy2 = pt2->y - pt0->y;
+	return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+
+
+bool inCorner(CvSeq* result)
+{
+	for (int pti = 0; pti < result->total; pti++)
+	{
+		uint i = ((CvPoint*)cvGetSeqElem(result, pti))->x;
+		uint j = ((CvPoint*)cvGetSeqElem(result, pti))->y;
+
+		//edge contours are inset by 1
+		uint right_edge = itsWidth - 2;
+		uint bottom_edge = itsHeight - 2;
+
+		if((i <= CORNER_TOLERANCE && j <= CORNER_TOLERANCE) ||
+			(i >= right_edge-CORNER_TOLERANCE && j <= CORNER_TOLERANCE) ||
+			(i <= CORNER_TOLERANCE && j >= bottom_edge-CORNER_TOLERANCE) ||
+			(i >= right_edge-CORNER_TOLERANCE && j >= bottom_edge-CORNER_TOLERANCE))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+
+// returns sequence of squares detected on the image.
+// the sequence is stored in the specified memory storage
+CvSeq* findSquares4(IplImage* img, CvMemStorage* storage)
+{
+	CvSize sz = cvSize( img->width & -2, img->height & -2 );
+	CvSeq* contours;
+	CvSeq* result;
+	int i, c, l, N = 11;
+	double s, t;
+	double img_area = sz.width * sz.height;
+	double max_area = img_area * 0.5;
+
+	IplImage* timg = cvCloneImage( img ); // make a copy of input image
+	IplImage* gray = cvCreateImage( sz, 8, 1 );
+	IplImage* pyr = cvCreateImage( cvSize(sz.width/2, sz.height/2), 8, 3 );
+	IplImage* tgray;
+
+	// create empty sequence that will contain points -
+	// 4 points per square (the square's vertices)
+	CvSeq* squares = cvCreateSeq( 0, sizeof(CvSeq), sizeof(CvPoint), storage );
+
+	// select the maximum ROI in the image
+	// with the width and height divisible by 2
+	//cvSetImageROI( timg, cvRect( max_roi_x1, max_roi_y1, max_roi_w, max_roi_h ));
+	cvSetImageROI( timg, cvRect( 0, 0, sz.width, sz.height ));
+
+	// down-scale and upscale the image to filter out the noise
+	cvPyrDown( timg, pyr, 7 );
+	cvPyrUp( pyr, timg, 7 );
+	tgray = cvCreateImage( sz, 8, 1 );
+
+	// TAKE OUT LATER
+	//	bottom 2 commented out by Mike Wei for conversion to ROS. 
+	//itsDisp.clear();
+	//inplacePaste(itsDisp, ipl2rgb(img), Point2D<int>(0, 0));
+
+	// find squares in every color plane of the image
+	for( c = 0; c < 3; c++ )
+	{
+		// extract the c-th color plane
+		cvSetImageCOI( timg, c+1 );
+		cvCopy( timg, tgray, 0 );
+
+		// try several threshold levels
+		for( l = 0; l < N; l++ )
+		{
+			// hack: use Canny instead of zero threshold level.
+			// Canny helps to catch squares with gradient shading
+			if( l == 0 )
+			{
+				// apply Canny. Take the upper threshold from slider
+				// and set the lower to 0 (which forces edges merging)
+				cvCanny( tgray, gray, 0, thresh, 5 );
+
+				// dilate canny output to remove potential
+				// holes between edge segments
+				cvDilate( gray, gray, 0, 1 );
+			} else {
+				// apply threshold if l!=0:
+				// tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+				cvThreshold( tgray, gray, (l+1)*255/N, 255, CV_THRESH_BINARY );
+			}
+
+			// find contours and store them all as a list
+			cvFindContours(gray, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
+
+			// test each contour
+			uint count =0;
+			while( contours ) {
+
+				// approximate contour with accuracy proportional
+				// to the contour perimeter
+				result = cvApproxPoly( contours, sizeof(CvContour), storage, CV_POLY_APPROX_DP, cvContourPerimeter(contours)*0.02, 0 );
+
+				// square contours should have 4 vertices after approximation
+				// relatively large area (to filter out noisy contours)
+				// and be convex.
+				// Note: absolute value of an area is used because
+				// area may be positive or negative - in accordance with the
+				// contour orientation
+
+				//also going to check that the area is smaller than max_area
+				double rect_area = fabs(cvContourArea(result,CV_WHOLE_SEQ));
+
+				if (result->total == 4
+            && rect_area > MIN_AREA
+            && rect_area < max_area
+            && cvCheckContourConvexity(result)
+            && !inCorner(result))
+				{
+					s = 0;
+
+					for (i = 0; i < 5; i++)
+					{
+						// find minimum angle between joint
+						// edges (maximum of cosine)
+						if (i >= 2)
+						{
+							t = fabs(angle(
+									(CvPoint*)cvGetSeqElem( result, i ),
+									(CvPoint*)cvGetSeqElem( result, i-2 ),
+									(CvPoint*)cvGetSeqElem( result, i-1 )));
+
+							s = s > t ? s : t;
+						}
+					}
+
+					// if cosines of all angles are small
+					// (all angles are ~90 degree) then write quandrange
+					// vertices to resultant sequence
+					if (s < angle_thresh)
+					{
+						//LINFO("RECTANGLE BABY");
+						for (i = 0; i < 4; i++)
+						{
+							cvSeqPush( squares, (CvPoint*)cvGetSeqElem( result, i ));
+						}
+					}
+				}
+
+				// take the next contour
+				contours = contours->h_next;
+				count++;
+			}
+		}
+	}
+
+	// release all the temporary images
+	cvReleaseImage( &gray );
+	cvReleaseImage( &pyr );
+	cvReleaseImage( &tgray );
+	cvReleaseImage( &timg );
+
+	return squares;
+}
+
+
+void findBin(const sensor_msgs::ImageConstPtr &img)
+{
+	/*
+	//LINFO("Image Received: %d", itsFrameCount);
+	bool isFwdCamera = false;
+	if(cameraId == "FwdCamera")
+		isFwdCamera = true;
+	*/
+	sensor_msgs::CvBridge bridge;
+/*
+	if(true)//img.initialized())
+	{
+
+		CvMemStorage* storage = cvCreateMemStorage(0);
+		//IplImage* img0 = img2ipl(img);
+		IplImage* img0 = bridge.imgMsgToCv(img);
+
+		CvSeq *cvsquares = findSquares4( img0, storage );
+
+		cvReleaseImage( &img0 );
+
+		RobotSimEvents::QuadrilateralIceVector quadVect;
+		ImageIceMod::QuadrilateralIce quad;
+		Point2D<int> avgCenter(0,0);
+		std::multimap<int,Point2D<int> > tempPoints;
+
+		// iterate over all the quadrilateral points found
+		for(int i=0; i < cvsquares->total; i++)
+		{
+			// get an individual point
+			Point2D<int> quadPoint;
+			quadPoint.i = ((CvPoint*)cvGetSeqElem(cvsquares, i))->x;
+			quadPoint.j = ((CvPoint*)cvGetSeqElem(cvsquares, i))->y;
+			
+			// add current point's position to running average of point positions
+			avgCenter += Point2D<int>(quadPoint.i,quadPoint.j);
+
+			// add the point to map, sorted by point Y-axis position
+			tempPoints.insert(make_pair(quadPoint.j,quadPoint));
+
+			//LINFO("tempPoints size: %d\n",tempPoints.size());
+
+			// if we have added the 4th point on the quadrilateral
+			if (tempPoints.size() == 4)
+			{
+				std::vector<Point2D<int> > tempVec;
+
+				for(std::map<int,Point2D<int > >::const_iterator it = tempPoints.begin();
+						it != tempPoints.end(); ++it)
+				{
+					tempVec.push_back(it->second);
+				}
+
+				// compare first two points to determine which is top left
+				// and which is top right
+				if(tempVec[0].i < tempVec[1].i)
+				{
+					quad.tl.i = tempVec[0].i;
+					quad.tr.i = tempVec[1].i;
+
+					quad.tl.j = tempVec[0].j;
+					quad.tr.j = tempVec[1].j;
+				}
+				else
+				{
+					quad.tr.i = tempVec[0].i;
+					quad.tl.i = tempVec[1].i;
+                
+					quad.tr.j = tempVec[0].j;
+					quad.tl.j = tempVec[1].j;
+				}
+
+				// compare second two points to determine bottom left and
+				// bottom right
+				if(tempVec[2].i < tempVec[3].i)
+				{
+					quad.bl.i = tempVec[2].i;
+					quad.br.i = tempVec[3].i;
+
+					quad.bl.j = tempVec[2].j;
+					quad.br.j = tempVec[3].j;
+				}
+				else
+				{
+					quad.br.i = tempVec[2].i;
+					quad.bl.i = tempVec[3].i;
+
+					quad.br.j = tempVec[2].j;
+					quad.bl.j = tempVec[3].j;
+				}
+
+
+				// divide by total number of averaged points
+				// to get current quad's center position
+				avgCenter /= Point2D<int>(4,4);
+				quad.center.i = avgCenter.i;
+				quad.center.j = avgCenter.j;
+
+				bool isDupe = false;
+
+				// make sure the quad's center is not too close
+				// to a prev. quad's center in order to avoid duplicates
+				for(uint j = 0; j < quadVect.size(); j++)
+				{
+					if(avgCenter.distance(Point2D<int>(quadVect[j].center.i,quadVect[j].center.j))
+						< MIN_CENTER_DIST)
+					{
+						isDupe = true;
+					}
+				}
+
+				// not dupe so add it to vector
+				if(!isDupe)
+				{
+					LineSegment2D vertLine = LineSegment2D((Point2D<int>(quad.tr.i,quad.tr.j) +
+						Point2D<int>(quad.tl.i,quad.tl.j))/2,
+						(Point2D<int>(quad.br.i,quad.br.j) +
+						Point2D<int>(quad.bl.i,quad.bl.j))/2);
+
+					LineSegment2D horizLine = LineSegment2D((Point2D<int>(quad.tl.i,quad.tl.j) +
+						Point2D<int>(quad.bl.i,quad.bl.j))/2,
+						(Point2D<int>(quad.tr.i,quad.tr.j) +
+						Point2D<int>(quad.br.i,quad.br.j))/2);
+					
+					float ratio = 0.0;
+					float angle = 0.0;
+					if(vertLine.length() > horizLine.length())
+					{
+						if(horizLine.length() > 0)
+							ratio = vertLine.length() / horizLine.length();
+
+						angle = vertLine.angle();
+					}
+					else
+					{
+						if(vertLine.length() > 0)
+							ratio = horizLine.length() / vertLine.length();
+
+						angle = horizLine.angle();
+					}
+
+					// change angle to degrees
+					angle = angle * (180/M_PI);
+
+					// normalize angle so that zero degrees is facing forawrd
+					// turning to the right is [0 -> 90]
+					// turning to the left is [0 -> -90]
+					if(angle < 0)
+						angle += 90;
+					else
+						angle += -90;
+
+					quad.ratio = ratio;
+					quad.angle = angle;
+					quadVect.push_back(quad);
+
+					// draw the quad on the image
+					drawLine(img,Point2D<int>(quad.tr.i,quad.tr.j),
+						Point2D<int>(quad.br.i,quad.br.j),
+						PixRGB<byte>(0,255,0),2);
+					drawLine(img,Point2D<int>(quad.br.i,quad.br.j),
+						Point2D<int>(quad.bl.i,quad.bl.j),
+						PixRGB<byte>(0,255,0),2);
+					drawLine(img,Point2D<int>(quad.bl.i,quad.bl.j),
+						Point2D<int>(quad.tl.i,quad.tl.j),
+						PixRGB<byte>(0,255,0),2);
+					drawLine(img,Point2D<int>(quad.tl.i,quad.tl.j),
+						Point2D<int>(quad.tr.i,quad.tr.j),
+						PixRGB<byte>(0,255,0),2);
+
+					char* str = new char[20];
+					sprintf(str,"%1.2f, %2.1f",ratio,angle);
+					writeText(img,Point2D<int>(quad.center.i,quad.center.j),str);
+					delete [] str;
+				}
+
+			// re-initialize for next quad
+			quad = ImageIceMod::QuadrilateralIce();
+			avgCenter = Point2D<int>(0,0);
+			tempPoints.clear();
+		}
+	}
+
+	itsOfs->writeRGB(img, "Vision Rectangle Image",
+		FrameInfo("Vision Rectangle Image", SRC_POS));
+
+	itsOfs->updateNext();
+
+	if (quadVect.size() > 0)
+	{
+		RobotSimEvents::VisionRectangleMessagePtr msg = new RobotSimEvents::VisionRectangleMessage;
+		msg->quads = quadVect;
+		msg->isFwdCamera = isFwdCamera;
+		this->publish("VisionRectangleMessageTopic", msg);
+	}
+
+	cvReleaseMemStorage( &storage );
+	}
+*/
+}
+
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "bin_finder");
+	ros::NodeHandle n;
+}
+
