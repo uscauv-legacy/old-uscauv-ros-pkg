6632f0f674757c44561310c98fee2353984c1636
Michael Montalbo <mmontalbo@gmail.com>
2010-07-04 19:57:52 +0000

    Started to add messaging for buoy_finder.

--- buoy_finder/src/buoy_finder.cpp
+++ buoy_finder/src/buoy_finder.cpp
@@ -5,15 +5,16 @@
 #include <tf/tf.h>
 #include <opencv/cv.h>
 #include <cv_bridge/CvBridge.h>
-#include "cvutility.h"
 #include <dynamic_reconfigure/server.h>
 #include <buoy_finder/BuoyFinderConfig.h>
+#include <buoy_finder/ColorBlobArray.h>
+#include "cvutility.h"
 #include "cvBlob/Blob.h"
 #include "cvBlob/BlobResult.h"
 
 using namespace cv;
 
-ros::Publisher *pos_pub; 
+ros::Publisher *buoy_pub; 
 image_transport::Publisher *dbg_img_pub;
 
 sensor_msgs::CvBridge bridge;
@@ -38,13 +39,10 @@ void reconfigureCallback(buoy_finder::BuoyFinderConfig &config, uint32_t level)
   s_min = config.s_min;
   s_max = config.s_max;
   //mode = config.mode;
-  mode = 0;
 }
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-  // ROS_INFO("Received image.");
-
   Mat img(bridge.imgMsgToCv(msg));
   IplImage iplImg = img;
 	
@@ -65,18 +63,6 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   blobs.Filter(blobs, B_EXCLUDE, CBlobGetArea(), B_GREATER, 10000);
 
 
-  CBlob biggestBlob;
-  blobs.GetNthBlob(CBlobGetArea(), 0, biggestBlob);
-
-  float blobX = (biggestBlob.MinX() + biggestBlob.MaxX()) / 2.0;
-  float blobY = (biggestBlob.MinY() + biggestBlob.MaxY()) / 2.0;
-
-  //Run a circle hough transform on the image to detect circular outlines
-  /*  vector<Vec3f> circles;
-  HoughCircles(redImg, circles, CV_HOUGH_GRADIENT,
-	       2, redImg.rows/4, 200, 100 );
-  */
-
   //Show the debug image if requested
   if(show_dbg_img != 0)
     {
@@ -85,19 +71,23 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
       if(blobs.GetNumBlobs() > 0)
 	{
+
+	  CBlob biggestBlob;
+	  blobs.GetNthBlob(CBlobGetArea(), 0, biggestBlob);
+	  
+	  float bArea = biggestBlob.Area();
+	  float bX = (biggestBlob.MinX() + biggestBlob.MaxX()) / 2.0;
+	  float bY = (biggestBlob.MinY() + biggestBlob.MaxY()) / 2.0;
+
 	  //Print out an annoying message so that we remember to disable the debug image
-	  ROS_INFO("Showing Debug Image - max blob at %f,%f area %f (%d total)", blobX, blobY, biggestBlob.Area(), blobs.GetNumBlobs());
-	  //      for( size_t i = 0; i < circles.size(); i++ )
-	  // 	{
-	  // 	  Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-	  // 	  int radius = cvRound(circles[i][2]);
-	  // 	  // draw the circle center
-	  Point center(blobX, blobY);
+	  ROS_INFO("Showing Debug Image - max blob at %f,%f area %f (%d total)", bX, bY, bArea, blobs.GetNumBlobs());
+
+	  Point center(bX, bY);
 	  rectangle( dbgImg, Point(biggestBlob.MinX(),biggestBlob.MinY()), Point(biggestBlob.MaxX(),biggestBlob.MaxY()), Scalar(0,255,0));
+ 	  // draw the circle center
 	  circle( dbgImg, center, 3, Scalar(0,255,0), -1, 8, 0 );
 	  // draw the circle outline
 	  circle( dbgImg, center, 5, Scalar(0,0,255), 3, 8, 0 );
-	  // 	}
 	}
 
       //Convert our debug image to an IplImage and send it out
@@ -105,11 +95,28 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
       dbg_img_pub->publish(bridge.cvToImgMsg(&iplImg));
     }
 
-  /* geometry_msgs::Vector3 buoy_pos;
-  buoy_pos.x = -1;
-  buoy_pos.y = -1;
-  buoy_pos.z = -1;
-  pos_pub->publish(buoy_pos);*/
+  buoy_finder::ColorBlobArray msgBlobs;
+
+  CBlob currentBlob;
+
+  for(int i = 0; i < blobs.GetNumBlobs(); i++)
+    {
+      blobs.GetNthBlob(CBlobGetArea(), i, currentBlob);
+      
+      float blobArea = currentBlob.Area();
+      float blobX = (currentBlob.MinX() + currentBlob.MaxX()) / 2.0;
+      float blobY = (currentBlob.MinY() + currentBlob.MaxY()) / 2.0;
+
+      buoy_finder::ColorBlob msgBlob;
+      msgBlob.X = blobX;
+      msgBlob.Y = blobY;
+      msgBlob.Area = blobArea;
+
+      
+      msgBlobs.push_back(msgBlob);
+    }
+
+  buoy_pub->publish(msgBlobs);
 }
 
 int main(int argc, char* argv[])
@@ -121,8 +128,8 @@ int main(int argc, char* argv[])
   //Register the show debug image parameter
   nh.param("show_dbg_img", show_dbg_img, 1);
 
-  //Register a publisher for the 3D position of the buoy
-  pos_pub = new ros::Publisher(nh.advertise<geometry_msgs::Vector3>("perception/buoy_pos", 1));
+  //Register a publisher for the position of the buoy
+  buoy_pub = new ros::Publisher(nh.advertise<buoy_finder::ColorBlobArray>("perception/buoy_pos", 1));
 
   //Register a publisher for a debug image
   dbg_img_pub = new image_transport::Publisher(it.advertise("perception/buoy_debug_image", 1));
