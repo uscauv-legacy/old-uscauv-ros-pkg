14f456e8a930cf39545084cdbb7225436fa744a4
Edward T. Kaszubski <ekaszubski@gmail.com>
2010-07-17 19:20:37 +0000

    Added shooter action to fsm.

--- seabee3_mission_control/src/ultra_basic_fsm.cpp
+++ seabee3_mission_control/src/ultra_basic_fsm.cpp
@@ -2,22 +2,26 @@
 //#include <seabee3_driver/SetDesiredRPY.h>
 #include <seabee3_driver/SetDesiredXYZ.h>
 #include <geometry_msgs/Twist.h>
+#include <seabee3_driver_base/FiringDeviceAction.h>
 
 int main(int argc, char * argv[])
 {
 	ros::init(argc, argv, "ultra_basic_fsm");
 	ros::NodeHandle n("~");
 	
-	int sleep1, dive_sleep, dive_depth, forward_speed;
+	int sleep1, dive_sleep, dive_depth, forward_speed, shooter_delay;
 	
 	n.param("sleep1", sleep1, 5);
 	n.param("dive_sleep", dive_sleep, 5);
+	n.param("shooter_delay", shooter_delay, 15);
 	
 	n.param("dive_depth", dive_depth, 950);
 	n.param("forward_speed", forward_speed, 80);
 	
 	ros::ServiceClient depth_srv = n.serviceClient<seabee3_driver::SetDesiredXYZ>("/seabee3/setDesiredXYZ");
+	ros::ServiceClient shooter_srv = n.serviceClient<seabee3_driver_base::FiringDeviceAction>("/seabee3/ShooterAction");
 	ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/seabee3/cmd_vel", 1);
+	seabee3_driver_base::FiringDeviceAction fireShooter;
 	
 	seabee3_driver::SetDesiredXYZ setDesiredXYZ;
 	setDesiredXYZ.request.Mask.z = 1;
@@ -51,12 +55,23 @@ int main(int argc, char * argv[])
 		{
 			if(dt.toSec() > dive_sleep)
 			{
+				lastTime = ros::Time::now();
 				mState = 2;
 			}
 		}
 		else if(mState == 2)
 		{
 			//drive forward
+			cmd_vel_pub.publish(cmd_vel);
+			if(dt.toSec() > shooter_delay)
+			{
+				lastTime = ros::Time::now();
+				mState = 3;
+				shooter_srv.call(fireShooter);
+			}
+		}
+		else if(mState == 3)
+		{
 			cmd_vel_pub.publish(cmd_vel);
 		}
 		
