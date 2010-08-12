a5ab01697aa20c614ae22ff520f3b6d2de813f6a
Edward T. Kaszubski <ekaszubski@gmail.com>
2010-08-12 12:29:07 +0000

    Added install.sh. Updated make.sh to work with any number/order of commands+targets. Added dynamic resolution to targets enclosed in $<>$ ie. --target '$<find -maxdepth 1 -type d ! -name "*.*">$' resolves to the list of directories without a . in the name.

---
+++ cturtle/install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo ""
+echo -n 'Install compiler tools? (y/n) '
+read res
+if [ "$res" == "y" ]; then
+	sudo apt-get update && sudo apt-get install build-essential python-yaml cmake subversion wget
+#	wget --no-check-certificate http://ros.org/rosinstall -O ~/rosinstall
+#	chmod 755 ~/rosinstall
+fi
+echo "----------"
+
+echo ""
+echo -n 'Install ros-cturtle-base? (y/n) '
+read res
+if [ "$res" == "y" ]; then
+	sudo sh -c 'echo "deb http://code.ros.org/packages/ros/ubuntu lucid main" > /etc/apt/sources.list.d/ros-latest.list'
+	wget http://code.ros.org/packages/ros.key -O - | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get install ros-cturtle-base
+fi
+echo "----------"
+
+echo ""
+echo -n 'Install pr2-controllers? (y/n) '
+read res
+if [ "$res" == "y" ]; then
+	sudo apt-get install ros-cturtle-pr2-controllers
+fi
+echo "----------"
+
+echo ""
+echo -n 'Configure environment variables? (y/n) '
+read res
+if [ "$res" == "y" ]; then
+	sudo sh -c 'echo "source /opt/ros/cturtle/setup.sh" >> ~/.bashrc'
+	. ~/.bashrc
+fi
+echo "----------"
+
+echo ""
+echo -n 'Download seabee3-ros-pkg? (y/n) '
+read res
+if [ "$res" == "y" ]; then
+	svn co https://seabee3-ros-pkg.googlecode.com/svn/trunk/cturtle/ ~/ros/stacks/seabee3-ros-pkg
+	rosstack profile && rospack profile
+fi
+echo "----------"
+
+echo ""
+echo -n 'Compile seabee3-ros-pkg? (y/n) '
+read res
+if [ "$res" == "y" ]; then
+	rosdep install seabee3-ros-pkg
+	rosmake seabee3-ros-pkg
+fi
+echo "----------"
+
+echo ""
+echo 'Done'
--- cturtle/make.sh
+++ cturtle/make.sh
@@ -1,30 +1,115 @@
 #!/bin/bash
-source /home/edward/ros/setup.sh
+SOURCE_PATH="/opt/ros/cturtle/setup.sh"
 
-opt=$1
-num=$#
-shift
+source $SOURCE_PATH
 
-if [ "$1" = "all" ]; then
-	if [ "$opt" = "strip-svn" ]; then
-		find -type d -name ".svn" | while read file; do
-			rm -rf $file
-		done
-	else
-		find -maxdepth 1 -type d ! -name ".*" | while read file; do
-			cd $file
-			make $opt
-			if [ "$opt" = "clean" ]; then
-				rm -rf bin
-			fi
-			cd ..
-		done
-	fi
-else
-	for ((i = 1; i < num; i++))
-	do
-		roscd $1
-		make $opt
+#bash make.sh -t all --cmd clean
+#bash make.sh -t all --cmd strip-svn
+#bash make.sh -t target1 target2 -c cmd opt1 opt2
+#bash make.sh -t xsens_node --cmd cd --cmd make -j5
+#bash make.sh -t seabee3_driver_base seabee3_driver --cmd rosmake
+
+mode=0
+n=0
+num_commands=0
+
+parseTargets()
+{
+	echo "parseTargets"
+	echo "$@"
+	n=0
+	while [ "$1" != "" ] && [ "$1" != "--foreach" ] && [ "$1" != "--command" ] && [ "$1" != "--target" ]; do
 		shift
+		targets[$n]=$1
+		n=$(($n + 1))
 	done
+}
+
+parseCommands()
+{
+	shift
+	echo "parseCommands"
+	echo ">$commands_raw[1]"
+	commands="$@"
+}
+
+usage()
+{
+	echo ""
+	echo "Usage: bash make.sh [--foreach] --command \"cmd1 cmd2 ..\" --target t1 t2 .."
+	echo ""
+	echo "Note on --command: @ -> list of all targets; $ -> Nth target"
+	echo ""
+	echo "Example1: bash make.sh --foreach --command \"cd $ && make -j5 && cd ..\" --target xsens_node seabee3_driver"
+	echo "Result: cd xsens_node && make -j5 && cd .. && cd seabee3_driver && make -j5 && cd .."
+	echo ""
+	echo "Example2: bash make.sh --command \"rosmake @ --pjobs 5\" --target xsens_node seabee3_driver"
+	echo "Result: rosmake xsens_node seabee3_driver --pjobs 5"
+}
+
+if [ $# -le 1 ]; then
+	usage
+	exit
 fi
+
+while [ "$1" != "" ]; do
+	case $1 in
+		--foreach )		shift
+						mode=1
+						;;
+		--command )		shift
+						if [ $num_commands -ge 1 ]; then
+							commands="$commands && "
+						fi
+						while [ "$1" != "" ] && [ "$1" != "--foreach" ] && [ "$1" != "--command" ] && [ "$1" != "--target" ]; do
+							commands="$commands $1"
+							shift
+						done
+						num_commands=$((num_commands + 1))
+						;;
+		--target )		shift
+						if [ "${1:0:2}" = "$<" ]; then
+							subprocess=$1
+#							echo ">$subprocess"
+							subprocess=${subprocess:2:$((${#subprocess} - 4))}
+#							echo ">>$subprocess"
+							val=($(bash -c "$subprocess"))
+#							echo ">>>${val[*]}"
+							shift
+							
+							for item in ${val[*]}; do
+#								echo "$item"
+								targets[$n]=$item
+								n=$(($n + 1))
+							done
+						else
+							while [ "$1" != "" ] && [ "$1" != "--foreach" ] && [ "$1" != "--command" ] && [ "$1" != "--target" ]; do
+								targets[$n]=$1
+								n=$(($n + 1))
+								shift
+							done
+						fi
+						;;
+		--help )		usage
+						exit
+						;;
+		* )				usage
+						exit
+	esac
+done
+
+echo "targets: ${targets[*]}"
+echo "commands: $commands"
+
+for target in ${targets[*]}; do
+	opt="$commands"
+	opt=${opt//%/ }
+	opt=${opt//$/$target}
+	opt=${opt//@/${targets[@]}}
+	echo "$opt"
+	echo "----------"
+	bash -c "source $SOURCE_PATH && $opt"
+	if [ "$mode" = "0" ]; then
+		break
+	fi
+done
