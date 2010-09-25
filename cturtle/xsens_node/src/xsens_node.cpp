2026ddf5f85108b2c79de3c2ce5e05b385849e43
Edward T. Kaszubski <ekaszubski@gmail.com>
2010-09-25 23:55:25 +0000

    Updated xsens_node to match microstrain api.

--- cturtle/xsens_node/src/xsens_node.cpp
+++ cturtle/xsens_node/src/xsens_node.cpp
@@ -38,11 +38,14 @@
 #include <ros/ros.h>
 #include <tf/tf.h> // for tf::Vector3
 #include <xsens/XSensDriver.h> // for XSensDriver
+#include <geometry_msgs/Vector3.h>
+#include <math.h> //for pow, sqrt
 //msgs
-#include <xsens_node/IMUData.h> // for outgoing IMUData
+#include <sensor_msgs/Imu.h> // for outgoing IMU data
+#include <std_msgs/Bool.h> //for Bool
 //srvs
 #include <xsens_node/CalibrateRPY.h> // for CalibrateRPY
-
+#include <std_srvs/Empty.h> //for Empty
 void operator +=( XSensDriver::Vector3 & v1, tf::Vector3 & v2 )
 {
 	v1.x += v2.getX();
@@ -77,9 +80,13 @@ private:
 	std::queue<tf::Vector3> ori_data_cache_;
 	tf::Vector3 ori_comp_, drift_comp_, drift_comp_total_;
 
+	std::string port_, frame_id_;
+	bool calibrated_, autocalibrate_, assume_calibrated_;
+	double orientation_stdev_, angular_velocity_stdev_, linear_acceleration_stdev_, max_drift_rate_;
+
 	ros::NodeHandle n_priv_;
-	ros::Publisher imu_pub_calib_;
-	ros::Publisher imu_pub_raw_;
+	ros::Publisher imu_pub_;
+	ros::Publisher is_calibrated_pub_;
 	ros::ServiceServer calibrate_rpy_drift_srv_;
 	ros::ServiceServer calibrate_rpy_ori_srv_;
 
@@ -90,8 +97,19 @@ public:
 	XSensNode( ros::NodeHandle & n ) :
 		n_priv_( "~" )
 	{
-		imu_pub_calib_ = n.advertise<xsens_node::IMUData> ( "data_calibrated", 1 );
-		imu_pub_raw_ = n.advertise<xsens_node::IMUData> ( "data_raw", 1 );
+		n_priv_.param( "port", port_, std::string( "/dev/ttyUSB0" ) );
+		n_priv_.param( "frame_id", frame_id_, std::string( "imu" ) );
+		n_priv_.param( "autocalibrate", autocalibrate_, true );
+		n_priv_.param( "orientation_stdev", orientation_stdev_, 0.035 );
+		n_priv_.param( "angular_velocity_stdev", angular_velocity_stdev_, 0.012 );
+		n_priv_.param( "linear_acceleration_stdev", linear_acceleration_stdev_, 0.098 );
+		n_priv_.param( "max_drift_rate", max_drift_rate_, 0.0001 );
+		n_priv_.param( "assume_calibrated", assume_calibrated_, false );
+
+		calibrated_ = false;
+
+		imu_pub_ = n.advertise<sensor_msgs::Imu> ( "data", 1 );
+		is_calibrated_pub_ = n.advertise<std_msgs::Bool> ( "is_calibrated", 1 );
 		calibrate_rpy_drift_srv_ = n.advertiseService( "calibrate_rpy_drift", &XSensNode::calibrateRPYDriftCB, this );
 		calibrate_rpy_ori_srv_ = n.advertiseService( "calibrate_rpy_ori", &XSensNode::calibrateRPYOriCB, this );
 
@@ -101,8 +119,6 @@ public:
 			ROS_WARN( "Failed to connect to IMU. Exiting..." );
 			return;
 		}
-
-		runRPYOriCalibration( 10 );
 	}
 
 	~XSensNode()
@@ -130,7 +146,24 @@ public:
 		}
 	}
 
-	void runRPYOriCalibration( int n )
+	void runFullCalibration()
+	{
+		runRPYOriCalibration();
+		runRPYDriftCalibration();
+		checkCalibration();
+	}
+
+	void checkCalibration()
+	{
+		double drift_rate = sqrt( pow( drift_comp_.x(), 2 ) + pow( drift_comp_.y(), 2 ) + pow( drift_comp_.z(), 2 ) );
+		calibrated_ = drift_rate > max_drift_rate_;
+
+		std_msgs::Bool is_calibrated_msg;
+		is_calibrated_msg.data = calibrated_;
+		is_calibrated_pub_.publish( is_calibrated_msg );
+	}
+
+	void runRPYOriCalibration( int n = 50 )
 	{
 		ori_comp_ *= 0.0; //reset the vector to <0, 0, 0>
 		for ( int i = 0; i < n && ros::ok(); i++ )
@@ -144,7 +177,7 @@ public:
 		ori_comp_ /= (double) ( -n );
 	}
 
-	void runRPYDriftCalibration( int n )
+	void runRPYDriftCalibration( int n = 50 )
 	{
 		drift_comp_ *= 0.0; //reset the vector to <0, 0, 0>
 		updateIMUData();
@@ -165,6 +198,8 @@ public:
 
 		ori_comp_ >> res.calibration;
 
+		checkCalibration();
+
 		return true;
 	}
 
@@ -174,26 +209,34 @@ public:
 
 		drift_comp_ >> res.calibration;
 
+		checkCalibration();
+
 		return true;
+	}
+
+	bool calibrateCB( std_srvs::Empty::Request & req, std_srvs::Empty::Response & res )
+	{
+		runFullCalibration();
 	}
 
 	void spin()
 	{
 		while ( ros::ok() )
 		{
-			xsens_node::IMUData msg_calib;
-			xsens_node::IMUData msg_raw;
+//			if ( autocalibrate_ && !calibrated_ )
+//				runFullCalibration();
+
+			sensor_msgs::Imu msg;
 
 			updateIMUData();
 
-			imu_driver_->accel_ >> msg_raw.accel;
-			imu_driver_->gyro_ >> msg_raw.gyro;
-			imu_driver_->mag_ >> msg_raw.mag;
-			imu_driver_->ori_ >> msg_raw.ori;
+			imu_driver_->accel_ >> msg.linear_acceleration;
+			imu_driver_->gyro_ >> msg.angular_velocity;
+			//imu_driver_->mag_ >> msg.mag;
 
-			imu_driver_->accel_ >> msg_calib.accel;
-			imu_driver_->gyro_ >> msg_calib.gyro;
-			imu_driver_->mag_ >> msg_calib.mag;
+			//imu_driver_->accel_ >> msg_calib.accel;
+			//imu_driver_->gyro_ >> msg_calib.gyro;
+			//imu_driver_->mag_ >> msg_calib.mag;
 
 			drift_comp_total_ += drift_comp_;
 
@@ -203,10 +246,19 @@ public:
 			imu_driver_->ori_ >> temp;
 			temp += ori_comp_;
 
-			temp >> msg_calib.ori;
+			tf::Quaternion ori ( temp.z(), temp.y(), temp.x() );
 
-			imu_pub_calib_.publish( msg_calib );
-			imu_pub_raw_.publish( msg_raw );
+			msg.orientation.w = ori.w();
+			msg.orientation.x = ori.x();
+			msg.orientation.y = ori.y();
+			msg.orientation.z = ori.z();
+
+			msg.angular_velocity_covariance[0] = msg.angular_velocity_covariance[4] = msg.angular_velocity_covariance[8] = angular_velocity_stdev_ * angular_velocity_stdev_;
+			msg.linear_acceleration_covariance[0] = msg.linear_acceleration_covariance[4] = msg.linear_acceleration_covariance[8] = linear_acceleration_stdev_ * linear_acceleration_stdev_;
+			msg.orientation_covariance[0] = msg.orientation_covariance[4] = msg.orientation_covariance[8] = orientation_stdev_ * orientation_stdev_;
+
+			imu_pub_.publish( msg );
+			//imu_pub_raw_.publish( msg_raw );
 			ros::spinOnce();
 			ros::Rate( 110 ).sleep();
 		}
