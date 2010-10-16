7729f75b33b70f9dbf86c9250dafa63f9b82b21f
toki303 <toki303>
2010-10-16 05:30:02 +0000

    initial commit of demo1_sung

---
+++ cturtle/demo1/demo1_sung/src/demo1.cpp
@@ -0,0 +1,35 @@
+#include <base_image_proc/base_image_proc.h>
+
+typedef BaseImageProcSettings::_DefaultReconfigureType _ConfigType;
+class Demo1: public BaseImageProc<base_image_proc::EmptyConfig>
+{
+public:
+	Demo1( ros::NodeHandle & nh ) :
+		BaseImageProc<_ConfigType> ( nh )
+	{
+
+	}
+	
+	virtual cv::Mat processImage( IplImage * ipl_img )
+	{
+		ROS_INFO( "Prroccesed Image" );
+		cv_img_ = cv::Mat( ipl_img );
+
+		cv::circle( cv_img_, cv::Point( 150, 270 ), 100, cv::Scalar( 0, 255, 67 ) );
+
+		return cv_img_;
+	}
+};
+
+int main( int argc, char ** argv )
+{
+	ros::init( argc, argv, "demo1_sung" );
+	ros::NodeHandle nh;
+
+	Demo1 demo1( nh );
+	demo1.spin();
+
+	return 0;
+
+}
+
