1e35fee072b7683066129d262091291bc9eae568
Dhruv Monga <dhruvmonga@gmail.com>
2011-02-05 01:41:38 +0000

    Fixed segmentation fault. The program compiles and runs fine.

--- cturtle/image_matcher/src/image_matcher_shape_matcher.cpp
+++ cturtle/image_matcher/src/image_matcher_shape_matcher.cpp
@@ -43,6 +43,7 @@
 #include <boost/thread/mutex.hpp>
 
 //using namespace std;
+//using namespace cv;
 
 typedef BaseNodeTypes::_DefaultReconfigureType _ReconfigureType;
 typedef image_matcher::MatchImage _ServiceType;
@@ -51,13 +52,48 @@ class ImageMatcher: public BaseImageProc<_ReconfigureType, _ServiceType>
 {
 
 public:
+	// File names for the bin objects
+	std::string axe_file_;
+	std::string clippers_file_;
+	std::string hammer_file_;
+	std::string machete_file_;
+
+	// Create grayscale image templates.
+	IplImage* gray_axe_;
+	IplImage* gray_clippers_;
+	IplImage* gray_hammer_ ;
+	IplImage* gray_machete_;
+	IplImage* gray_image_;
+
 	ImageMatcher( ros::NodeHandle & nh ) :
 		BaseImageProc<_ReconfigureType, _ServiceType> ( nh, "match_images" ), image_size_initialized_( false ), images_loaded_( false )
 	{
-		nh.param( "axe", axe_file_, std::string( "axe.png" ) );
-		nh.param( "clippers", clippers_file_, std::string( "clippers.png" ) );
-		nh.param( "hammer", hammer_file_, std::string( "hammer.png" ) );
-		nh.param( "machete", machete_file_, std::string( "machete.png" ) );
+		/*CvMat axe_mat;
+		CvMat clippers_mat;
+		CvMat hammer_mat;
+		CvMat machete_mat;
+
+		axe_mat = imread(axe.jpeg, 1);
+		clippers_mat = imread(clippers.jpeg, 1);
+		hammer_mat = imread(hammer.jpeg, 1);
+		machete_mat = imread(machete.jpeg, 1);*/
+
+		/*axe_file_ = "axe.jpeg";
+		clippers_file_ = "clippers.jpeg";
+		hammer_file_ = "hammer.jpeg";
+		machete_file_ = "machete.jpeg";*/
+
+		nh.param( "axe", axe_file_, std::string( "../axe.jpeg" ) );
+		nh.param( "clippers", clippers_file_, std::string( "../clippers.jpeg" ) );
+		nh.param( "hammer", hammer_file_, std::string( "../hammer.	jpeg" ) );
+		nh.param( "machete", machete_file_, std::string( "../machete.jpeg" ) );
+		/*axe_file_ = "axe.jpeg";
+		clippers_file_ = "clippers.jpeg";
+		hammer_file_ = "hammer.jpeg";
+		machete_file_ = "machete.jpeg";*/
+		//ROS_ERROR("machete_file_ is %s", machete_file_);
+
+
 
 		loadPlusConvertImages();
 		ROS_INFO( "Finished loading and converting competition templates" );
@@ -70,18 +106,7 @@ public:
 		cvReleaseImage( &gray_hammer_ );
 		cvReleaseImage( &gray_machete_ );
 	}
-	// File names for the bin objects
-	std::string axe_file_;
-	std::string clippers_file_;
-	std::string hammer_file_;
-	std::string machete_file_;
 
-	// Create grayscale image templates.
-	IplImage * gray_axe_;
-	IplImage * gray_clippers_;
-	IplImage * gray_hammer_;
-	IplImage * gray_machete_;
-	IplImage * gray_image_;
 	bool image_size_initialized_;
 	bool images_loaded_;
 	const static uint match_method_ = CV_CONTOURS_MATCH_I3;
@@ -126,18 +151,33 @@ public:
 			IplImage* machete_img;
 
 
+			// THIS IS ONLY A TEST!!!!!!!!!!
+			//axe_file_ = "axe.jpeg";
+			//ROS_ERROR ("axe file is %s, clippers file is %s", axe_file_, clippers_file_);
+			//END TEST
+
 			// Extract images from their respective files
-			axe_img = cvLoadImage( axe_file_.c_str() );
-			clippers_img = cvLoadImage( clippers_file_.c_str() );
-			hammer_img = cvLoadImage( hammer_file_.c_str() );
-			machete_img = cvLoadImage( machete_file_.c_str() );
+			axe_img = cvLoadImage( axe_file_.c_str(), 1 );
+			//if(!axe_img_){ROS_ERROR ("COULD NOT LOAD IMAGE! ");}
+			//if(!axe_img){ROS_ERROR ("COULD NOT LOAD IMAGE!");}
+			clippers_img = cvLoadImage( clippers_file_.c_str(), 1 );
+			hammer_img = cvLoadImage( hammer_file_.c_str(), 1 );
+			machete_img = cvLoadImage( machete_file_.c_str(), 1 );
 
+			//ROS_ERROR ("axe file is %s, clippers file is %s", axe_file_, clippers_file_);
 
+			//ROS_ERROR ("pointer clippers_img is %p", clippers_img);
+/*
+			IplImage* gray_axe_;
+			IplImage* gray_clippers_;
+			IplImage* gray_hammer_;
+			IplImage* gray_machete_;*/
 			// Create grayscale image templates.
-			gray_axe_ = cvCreateImage( CvSize( axe_img->width, axe_img->height ), IPL_DEPTH_8U, 1 );
-			gray_clippers_ = cvCreateImage( CvSize( clippers_img->width, clippers_img->height ), IPL_DEPTH_8U, 1 );
-			gray_hammer_ = cvCreateImage( CvSize( hammer_img->width, hammer_img->height ), IPL_DEPTH_8U, 1 );
-			gray_machete_ = cvCreateImage( CvSize( machete_img->width, machete_img->height ), IPL_DEPTH_8U, 1 );
+			//ROS_ERROR ("the width of axe image is %i", axe_img->width);
+			gray_axe_ = cvCreateImage( cvSize( axe_img->width, axe_img->height ), IPL_DEPTH_8U, 1 );
+			gray_clippers_ = cvCreateImage( cvSize( clippers_img->width, clippers_img->height ), IPL_DEPTH_8U, 1 );
+			gray_hammer_ = cvCreateImage( cvSize( hammer_img->width, hammer_img->height ), IPL_DEPTH_8U, 1 );
+			gray_machete_ = cvCreateImage( cvSize( machete_img->width, machete_img->height ), IPL_DEPTH_8U, 1 );
 
 
 			// Convert all images to grayscale
@@ -158,7 +198,7 @@ public:
 	{
 		if ( !image_size_initialized_ )
 		{
-			gray_image_ = cvCreateImage( CvSize( ipl_img->width, ipl_img->height ), IPL_DEPTH_8U, 1 );
+			gray_image_ = cvCreateImage( cvSize( ipl_img->width, ipl_img->height ), IPL_DEPTH_8U, 1 );
 		}
 
 		resp.percent_match = matchImageCall( ipl_img, req );
