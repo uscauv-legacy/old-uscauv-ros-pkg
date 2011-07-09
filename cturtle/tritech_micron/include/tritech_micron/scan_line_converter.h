60b6f232c15cd8e629802cb3991ab9b64f846c1e
Edward T. Kaszubski <ekaszubski@gmail.com>
2011-07-09 00:11:21 +0000

    Added a point-cloud publisher to scan_line_converter. Updated launch files and rviz config file.

--- cturtle/tritech_micron/include/tritech_micron/scan_line_converter.h
+++ cturtle/tritech_micron/include/tritech_micron/scan_line_converter.h
@@ -40,6 +40,7 @@
 #include <common_utils/math.h>
 #include <tritech_micron/ScanLine.h>
 #include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud.h>
 #include <tritech_micron/ScanLineConverterConfig.h>
 
 typedef unsigned int _DimType;
@@ -49,25 +50,42 @@ typedef BaseNode<_ReconfigureType> _BaseNode;
 typedef tritech_micron::ScanLine _ScanLineMsgType;
 typedef tritech_micron::IntensityBin _IntensityBinMsgType;
 typedef sensor_msgs::LaserScan _LaserScanMsgType;
+typedef sensor_msgs::PointCloud _PointCloudMsgType;
 
 typedef float _StepType;
 typedef float _AngleType;
 typedef std::vector<unsigned char> _IntensityBinsRawType;
 
-class ScanLineConverter : public _BaseNode
+class ScanLineConverter: public _BaseNode
 {
 public:
 	ros::Subscriber scan_line_sub_;
 	ros::Publisher laser_scan_pub_;
+	ros::Publisher point_cloud_pub_;
 
-	ScanLineConverter( ros::NodeHandle & nh ) : _BaseNode( nh )
+	ScanLineConverter( ros::NodeHandle & nh ) :
+			_BaseNode( nh )
 	{
-		scan_line_sub_ = nh_local_.subscribe( "scan_line", 1, &ScanLineConverter::scanLineCB, this );
-		laser_scan_pub_ = nh_local_.advertise<_LaserScanMsgType>( "laser_scan", 1 );
+		scan_line_sub_ = nh_local_.subscribe( "scan_line",
+		                                      1,
+		                                      &ScanLineConverter::scanLineCB,
+		                                      this );
+
+		laser_scan_pub_ = nh_local_.advertise<_LaserScanMsgType>( "laser_scan",
+		                                                          1 );
+
+		point_cloud_pub_ = nh_local_.advertise<_PointCloudMsgType>( "point_cloud",
+		                                                           1 );
 		initCfgParams();
 	}
 
 	void scanLineCB( const _ScanLineMsgType::ConstPtr & scan_line_msg )
+	{
+		publishLaserScan( scan_line_msg );
+		publishPointCloud( scan_line_msg );
+	}
+
+	void publishLaserScan( const _ScanLineMsgType::ConstPtr & scan_line_msg )
 	{
 		_LaserScanMsgType::Ptr laser_scan_msg( new _LaserScanMsgType );
 		laser_scan_msg->header = scan_line_msg->header;
@@ -79,15 +97,15 @@ public:
 		//laser_scan_msg->time_increment = 0.1;
 		//laser_scan_msg->scan_time = 0.1;
 
-		for( _DimType i = 0; i < scan_line_msg->bins.size(); ++i )
+		for ( _DimType i = 0; i < scan_line_msg->bins.size(); ++i )
 		{
-			if( !reconfigure_params_.use_threshold || scan_line_msg->bins[i].intensity >= reconfigure_params_.min_intensity_threshold )
+			if ( !reconfigure_params_.use_laser_threshold || scan_line_msg->bins[i].intensity >= reconfigure_params_.min_laser_intensity_threshold )
 			{
 				laser_scan_msg->intensities.push_back( scan_line_msg->bins[i].intensity / 255.0 );
 				laser_scan_msg->ranges.push_back( scan_line_msg->bins[i].distance );
 			}
 
-			if( reconfigure_params_.use_threshold && scan_line_msg->bins[i].intensity >= reconfigure_params_.min_intensity_threshold )
+			if ( reconfigure_params_.use_laser_threshold && scan_line_msg->bins[i].intensity >= reconfigure_params_.min_laser_intensity_threshold )
 			{
 				// for visualization, at least two laser scans must be sent out...for whatever reason
 				laser_scan_msg->intensities.push_back( scan_line_msg->bins[i].intensity / 255.0 );
@@ -96,7 +114,37 @@ public:
 			}
 		}
 
-		if( laser_scan_msg->ranges.size() > 0 ) laser_scan_pub_.publish( laser_scan_msg );
+		if ( laser_scan_msg->ranges.size() > 0 ) laser_scan_pub_.publish( laser_scan_msg );
+	}
+
+	void publishPointCloud( const _ScanLineMsgType::ConstPtr & scan_line_msg )
+	{
+		_PointCloudMsgType::Ptr point_cloud_msg( new _PointCloudMsgType );
+		point_cloud_msg->header = scan_line_msg->header;
+
+		point_cloud_msg->points.reserve( scan_line_msg->bins.size() );
+
+		sensor_msgs::ChannelFloat32 channel;
+		channel.name = "intensity";
+		channel.values.reserve( scan_line_msg->bins.size() );
+
+		for ( _DimType i = 0; i < scan_line_msg->bins.size(); ++i )
+		{
+			if( !reconfigure_params_.use_point_cloud_threshold || scan_line_msg->bins[i].intensity >= reconfigure_params_.min_point_cloud_intensity_threshold )
+			{
+				geometry_msgs::Point32 point;
+				point.x = scan_line_msg->bins[i].distance * cos( math_utils::degToRad( scan_line_msg->angle ) );
+				point.y = scan_line_msg->bins[i].distance * sin( math_utils::degToRad( scan_line_msg->angle ) );
+				point.z = 0;
+				point_cloud_msg->points.push_back( point );
+
+				channel.values.push_back( scan_line_msg->bins[i].intensity / 255.0 );
+			}
+		}
+
+		point_cloud_msg->channels.push_back( channel );
+
+		point_cloud_pub_.publish( point_cloud_msg );
 	}
 };
 
