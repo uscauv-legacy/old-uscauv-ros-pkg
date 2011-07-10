7f2d6493b6b6c3b4512f2200501446ee043d203c
John Ohollaren <john.p.ohollaren@gmail.com>
2011-07-10 03:34:16 +0000

    updated outline for the stabilizer

--- cturtle/seabee3_stabilizer/include/seabee3_stabilizer.h
+++ cturtle/seabee3_stabilizer/include/seabee3_stabilizer.h
@@ -1,3 +1,30 @@
+
 #include <string>
 
+using namespace std;
 
+class Stabilizer {
+
+/*
+  
+  1. grab IMU msgs
+
+  2. compare IMU msgs with nominal/stable values
+
+  3. if error is > than some threshold,
+    
+    a. create a thruster command to correct it
+
+    b. publish corrective thruster command
+
+*/
+
+  Stabilizer() {
+
+  }
+  
+  ~Stabilizer() {
+
+  }
+
+}
--- cturtle/seabee3_stabilizer/src/seabee3_stabilizer.cpp
+++ cturtle/seabee3_stabilizer/src/seabee3_stabilizer.cpp
@@ -1,7 +1,5 @@
 #include "seabee3_stabilizer.h"
 
-using namespace std;
-
 int main( int argc, char* argv[] )
 {
 	return 0;
