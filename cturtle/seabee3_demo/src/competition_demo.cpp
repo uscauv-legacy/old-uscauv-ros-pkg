21de9c06cbb19c421e5dab4513c06bf58159c25f
Rand Voorhies <rand.voorhies@gmail.com>
2011-07-16 11:26:05 +0000

    Fixing up kill switch logic

--- cturtle/seabee3_demo/src/competition_demo.cpp
+++ cturtle/seabee3_demo/src/competition_demo.cpp
@@ -20,12 +20,18 @@ class CompetitionDemo : public BaseNode<>
     ros::Subscriber kill_switch_sub_;
     ros::ServiceClient set_desired_pose_cli_;
     ros::ServiceClient reset_pose_cli_;
+    ros::ServiceClient set_enabled_landmarks_cli_;
+
+    ros::Subscriber landmarks_sub_;
 
     tf::Transform current_pose_;
+    tf::Transform landmark_pose_;
 
-    enum FSMState_t {waiting_for_start, running, killing};
+    enum FSMState_t {waiting_for_start, starting, running, killing};
     FSMState_t state_;
     std::thread FSMthread_;
+
+    bool landmark_found_;
 
     ////////////////////////////////
     // Run Parameters
@@ -38,6 +44,8 @@ class CompetitionDemo : public BaseNode<>
     // ######################################################################
     CompetitionDemo( ros::NodeHandle & nh ) : BaseNode<> ( nh )
   {
+    state_ = waiting_for_start;
+
     nh_local_.param( "error_threshold", error_threshold_, 0.1 );
     nh_local_.param( "depth", depth_, 1.7 );
     nh_local_.param( "distance_to_gate", distance_to_gate_, 10.0 );
@@ -45,7 +53,18 @@ class CompetitionDemo : public BaseNode<>
     kill_switch_sub_ = nh_local_.subscribe( "/seabee3/kill_switch", 2, &CompetitionDemo::killSwitchCB, this );
     set_desired_pose_cli_ = nh_local_.serviceClient<seabee3_common::SetDesiredPose> ( "/seabee3/set_desired_pose" );
     reset_pose_cli_ = nh_local_.serviceClient<std_srvs::Empty> ( "/seabee3/reset_pose" );
+
+    //landmarks_sub_ = nh_local_.subscribe( "/landmark_finder/landmarks", 3, &CompetitionDemo::landmarksCB, this );
+    //set_enabled_landmarks_cli_ = nh_local_.serviceClient<landmark_finder::SetEnabledLandmarks>(
+    //    "/landmark_finder/set_enabled_landmarks");
   }
+
+    //// ######################################################################
+    //void landmarksCB( const localization_defs::LandmarkArray::ConstPtr & landmark_msg )
+    //{
+    //  // TODO Set the landmark_pose_
+    //  ROS_INFO("Got new landmarks");
+    //}
 
     // ######################################################################
     void killSwitchCB( const seabee3_driver_base::KillSwitch::ConstPtr & kill_switch_msg )
@@ -53,10 +72,15 @@ class CompetitionDemo : public BaseNode<>
       static bool last_killed_state_ = kill_switch_msg->is_killed;
       bool current_killed_state = kill_switch_msg->is_killed;
 
-      if(last_killed_state_ == false && current_killed_state == true)
+      if(state_ == running && last_killed_state_ == false && current_killed_state == true)
       {
         ROS_INFO("Kill Switch CB: Killing");
         state_ = killing;
+      }
+      else if(state_ == waiting_for_start && last_killed_state_ == true && current_killed_state == false)
+      {
+        ROS_INFO("Kill Switch CB: Starting");
+        state_ = starting;
       }
       last_killed_state_ = current_killed_state;
     }
@@ -89,9 +113,9 @@ class CompetitionDemo : public BaseNode<>
       // Dive
       //////////////////////////////
       ROS_INFO("Diving...");
-      double roll, pitch, yaw;
-      current_pose_.getBasis().getEulerYPR( yaw, pitch, roll );
       {
+        double roll, pitch, yaw;
+        current_pose_.getBasis().getEulerYPR( yaw, pitch, roll );
         seabee3_common::SetDesiredPose set_desired_pose_;
         set_desired_pose_.request.ori.values.z = yaw;
         set_desired_pose_.request.ori.mask.z   = 1;
@@ -107,8 +131,8 @@ class CompetitionDemo : public BaseNode<>
       ROS_INFO("Cruising to gate...");
       {
         seabee3_common::SetDesiredPose set_desired_pose_;
-        set_desired_pose_.request.ori.values.x = distance_to_gate_;
-        set_desired_pose_.request.ori.mask.x   = 1;
+        set_desired_pose_.request.pos.values.x = distance_to_gate_;
+        set_desired_pose_.request.pos.mask.x   = 1;
         set_desired_pose_cli_.call( set_desired_pose_.request, set_desired_pose_.response );
       }
       waitForPose();
@@ -116,6 +140,22 @@ class CompetitionDemo : public BaseNode<>
       //////////////////////////////
       // Begin search for buoy
       //////////////////////////////
+      // TODO: all of this:
+      // 1) loop through list of landmarks:
+      //    a) set enabled landmark
+      //    b) spin!
+      landmark_found_ = false;
+      while(!landmark_found_)
+      {
+        double roll, pitch, yaw;
+        current_pose_.getBasis().getEulerYPR( yaw, pitch, roll );
+        seabee3_common::SetDesiredPose set_desired_pose_;
+        set_desired_pose_.request.ori.values.z = yaw + M_PI/16;
+        set_desired_pose_.request.ori.mask.z   = 1;
+        set_desired_pose_cli_.call( set_desired_pose_.request, set_desired_pose_.response );
+        waitForPose();
+      }
+      //    c) head towards landmark until within error tolerance:
 
 
     }
@@ -125,7 +165,7 @@ class CompetitionDemo : public BaseNode<>
     {
       tf_utils::fetchTfFrame( current_pose_, "/landmark_map", "/seabee3/base_link" ); 
 
-      if(state_ == waiting_for_start)
+      if(state_ == starting)
       {
         ROS_INFO("Spin Once: Starting Run");
         FSMthread_ = std::thread(std::bind(&CompetitionDemo::letsDoThis, this));
@@ -138,10 +178,7 @@ class CompetitionDemo : public BaseNode<>
         state_ = waiting_for_start;
         ROS_INFO("Spin Once: Killed.. Waiting for start");
       }
-
-
     }
-
 };
 
 
