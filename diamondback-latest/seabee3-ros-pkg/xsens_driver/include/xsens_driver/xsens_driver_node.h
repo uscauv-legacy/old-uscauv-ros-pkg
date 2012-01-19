c7cd769e10617bd54977b622325815bf27330149
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-01-19 22:25:49 +0000

    Added xsens_driver with skeleton code. xsens_driver/xsens_node.cpp needs to be ported to quickdev and placed in nodes/xsens_driver.cpp, include/xsens_driver/xsens_driver_node.h.

---
+++ diamondback-latest/seabee3-ros-pkg/xsens_driver/include/xsens_driver/xsens_driver_node.h
@@ -0,0 +1,93 @@
+/***************************************************************************
+ *  include/xsens_driver/xsens_driver_node.h
+ *  --------------------
+ *
+ *  Copyright (c) 2011, Edward T. Kaszubski ( ekaszubski@gmail.com )
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following disclaimer
+ *    in the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of seabee3-ros-pkg nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **************************************************************************/
+
+#ifndef XSENSDRIVER_XSENSDRIVER_H_
+#define XSENSDRIVER_XSENSDRIVER_H_
+
+#include <quickdev/node.h>
+
+// declare a node called xsens_driverNode
+// a quickdev::RunablePolicy is automatically prepended to the list of policies our node will use
+// to use more policies, simply list them here:
+//
+// QUICKDEV_DECLARE_NODE( xsens_driver, SomePolicy1, SomePolicy2 )
+//
+QUICKDEV_DECLARE_NODE( xsens_driver )
+
+// declare a class called xsens_driverNode
+//
+QUICKDEV_DECLARE_NODE_CLASS( xsens_driver )
+{
+    // variable initializations can be appended to this constructor as a comma-separated list:
+    //
+    // QUICKDEV_DECLARE_NODE_CONSTRUCTOR( xsens_driver ), member1_( some_value ), member2_( some_other_value ){}
+    //
+    QUICKDEV_DECLARE_NODE_CONSTRUCTOR( xsens_driver )
+    {
+        //
+    }
+
+    // this function is called by quickdev::RunablePolicy after all policies are constructed but just before the main loop is started
+    // all policy initialization should be done here
+    //
+    QUICKDEV_SPIN_FIRST()
+    {
+        // say we had a policy called _SomePolicy that looked for the meta-parameter "some_value1_param" of type SomeType and
+        // "some_value2_param" of type SomeOtherType in its init function
+        // we can create those meta-params here and then pass them to all policies using initAll():
+        //
+        // initAll( "some_value1_param", SomeType(), "some_value2_param", SomeOtherType() );
+        //
+        // or we can pass those meta-params only to _SomePolicy using its init() function:
+        //
+        // _SomePolicy::init( "some_value1_param", SomeType(), "some_value2_param", SomeOtherType() );
+        //
+        // if we don't want to initialize all policies and use their default values, we can simply call initAll() with no arguments
+        // note that most initable policies won't function properly unless their init() functions are called
+        // therefore, to get the default behavior from all policies, be sure to call initAll()
+        //
+        initAll();
+    }
+
+    // this opitonal function is called by quickdev::RunablePolicy at a fixed rate (defined by the ROS param _loop_rate)
+    // most updateable policies should have their update( ... ) functions called within this context
+    //
+    QUICKDEV_SPIN_ONCE()
+    {
+        //
+    }
+};
+
+#endif // XSENSDRIVER_XSENSDRIVER_H_
