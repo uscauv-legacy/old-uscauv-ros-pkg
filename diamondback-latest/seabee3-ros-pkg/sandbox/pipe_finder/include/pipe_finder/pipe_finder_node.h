e01c38b16d504a208d4fd450240786553a3d0b2e
Kathryn Yu <ubekeyk@gmail.com>
2012-04-12 23:15:32 +0000

    Updated pipe finder to compiling state.

--- diamondback-latest/seabee3-ros-pkg/sandbox/pipe_finder/include/pipe_finder/pipe_finder_node.h
+++ diamondback-latest/seabee3-ros-pkg/sandbox/pipe_finder/include/pipe_finder/pipe_finder_node.h
@@ -38,7 +38,17 @@
 #define CVCLOSE_ITR 1
 
 #include <quickdev/node.h>
-#include <quickdev/image_proc_policy.h>
+
+#include <quickdev/multi_subscriber.h>
+
+#include <quickdev/opencv_conversion.h>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#include <seabee3_common/NamedImageArray.h>
+
+typedef seabee3_common::NamedImageArray _NamedImageArrayMsg;
 
 // declare a node called PipeFinderNode
 // a quickdev::RunablePolicy is automatically prepended to the list of policies our node will use
@@ -46,12 +56,13 @@
 //
 // QUICKDEV_DECLARE_NODE( PipeFinder, SomePolicy1, SomePolicy2 )
 //
-QUICKDEV_DECLARE_NODE( PipeFinder, quickdev::ImageProcPolicy )
+QUICKDEV_DECLARE_NODE( PipeFinder )
 
 // declare a class called PipeFinderNode
 //
 QUICKDEV_DECLARE_NODE_CLASS( PipeFinder )
 {
+	ros::MultiSubscriber<> multi_sub_;
     // variable initializations can be appended to this constructor as a comma-separated list:
     //
     // QUICKDEV_DECLARE_NODE_CONSTRUCTOR( PipeFinder ), member1_( some_value ), member2_( some_other_value ){}
@@ -81,16 +92,23 @@ QUICKDEV_DECLARE_NODE_CLASS( PipeFinder )
         // note that most initable policies won't function properly unless their init() functions are called
         // therefore, to get the default behavior from all policies, be sure to call initAll()
         //
-        initAll();
+        //initAll();
+        
+        initPolicies<quickdev::policy::ALL>();
+
+        QUICKDEV_GET_RUNABLE_NODEHANDLE( nh_rel );
+
+        multi_sub_.addSubscriber( nh_rel, "images", &PipeFinderNode::imagesCB, this );
     }
 
     // this opitonal function is called by quickdev::RunablePolicy at a fixed rate (defined by the ROS param _loop_rate)
     // most updateable policies should have their update( ... ) functions called within this context
     //
     
-    IMAGE_PROC_PROCESS_IMAGE( image_ptr )
+    /*IMAGE_PROC_PROCESS_IMAGE( image_ptr )
     {
 		const float ASPECT_RATIO_BOUNDARY = 1.5;
+		const float perimScale = 10;
 		cv::Mat input = image_ptr->image;
 		cv::Mat gray_input;
 		std::vector<CvBox2D> usable_boxes;
@@ -108,23 +126,11 @@ QUICKDEV_DECLARE_NODE_CLASS( PipeFinder )
 		cvtColor(input, gray_input, CV_RGB2GRAY);
 		
 		//clean up image
-		cvMorphologyEx( 
-						&IplImage( gray_input ), 
-						&IplImage( gray_input ), 
-						0, 
-						0, 
-						CV_MOP_OPEN, 
-						CVCLOSE_ITR 
-					  );
-		cvMorphologyEx( 
-						&IplImage( gray_input ),
-						&IplImage( gray_input ), 
-						0, 
-						0, 
-						CV_MOP_CLOSE, 
-						CVCLOSE_ITR 
-					   );
-		//begin contour finder
+		cv::Mat elem = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5,5));
+		cv::morphologyEx(gray_input, gray_input, cv::MORPH_OPEN, elem, cv::Point(-1,-1), 3);
+		//cv::morphologyEx(gray_input, gray_input, elem, cv::MORPH_OPEN, cv::Point(-1,-1), 3);
+		
+				//begin contour finder
 		cvFindContours( 
 						&IplImage( gray_input ), 
 						storage, &contours, 
@@ -147,24 +153,120 @@ QUICKDEV_DECLARE_NODE_CLASS( PipeFinder )
 					
 				if( aspect_ratio >= ASPECT_RATIO_BOUNDARY )
 				{
-					usable_boxes.push_back( box_to_check );
-					cvEllipseBox( 
-								  &IplImage( input ), 
-								  box_to_check, 
-								  CV_RGB( 255, 0, 0 ), 
-								  1, 
-								  8, 
-								  0 
-								 );
-				}
-			}
+				 //sort usable contours by their perimeter, discard those that are too small					
+					double perimeterbound = (gray_input.rows + gray_input.cols)/perimScale; 
+					double totalperimeter = box_to_check.size.width * 2 + box_to_check.size.height * 2;
+					
+					if (totalperimeter >= perimeterbound)
+					{
+						usable_boxes.push_back( box_to_check );
+						cvEllipseBox( 
+									  &IplImage( input ), 
+									  box_to_check, 
+									  CV_RGB( 255, 0, 0 ), 
+									  3, 
+									  8, 
+									  0 
+									 );
+					 } //end perimeter check
+				} //end aspect ratio check
+			} //end contour total check
+			
 			contours = contours->h_next;
 		}
 		//landmarks::Pipe pipe( cv::Point3( x, y, z ), rotation );
 		//multi_pub_.publish( "landmarks", pipe.createMsg() );
 		cv::imshow( "Found_boxes", input );
         cvWaitKey( 20 );
+	}*/
+	
+	QUICKDEV_DECLARE_MESSAGE_CALLBACK( imagesCB, _NamedImageArrayMsg )
+    {
+        for( auto named_image_msg = msg->images.cbegin(); named_image_msg != msg->images.cend(); ++named_image_msg )
+        {
+			// named_image_msg->name == "orange"
+			if( strcmp(named_image_msg->name.c_str(), "orange") )
+			{
+				cv::namedWindow( named_image_msg->name, 0 );
+
+				auto const cv_image_ptr = quickdev::opencv_conversion::fromImageMsg( named_image_msg->image );
+				cv::Mat const & input = cv_image_ptr->image;
+
+				PRINT_INFO( "Got %s image %ix%i", named_image_msg->name.c_str(), input.size().width, input.size().height );
+
+				cv::imshow( named_image_msg->name, input );
+					
+				const float ASPECT_RATIO_BOUNDARY = 1.5;
+				const float perimScale = 10;
+				//cv::Mat input = image_ptr->image;
+				cv::Mat gray_input;
+				std::vector<CvBox2D> usable_boxes;
+				CvMemStorage* storage;
+				CvSeq* contours;
+				CvBox2D box_to_check;
+				storage = cvCreateMemStorage(0);
+				contours = cvCreateSeq( 
+										CV_SEQ_ELTYPE_POINT, 
+										sizeof( CvSeq ), 
+										sizeof( CvPoint ), 
+										storage 
+									  );
+				//make 8uc1 Mat
+				cvtColor(input, gray_input, CV_RGB2GRAY);
+
+				cv::Mat elem = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5,5));
+				cv::morphologyEx(gray_input, gray_input, cv::MORPH_OPEN, elem, cv::Point(-1,-1), 3);
+				
+				//begin contour finder
+				cvFindContours( 
+								&IplImage( gray_input ), 
+								storage, &contours, 
+								sizeof( CvContour ), 
+								CV_RETR_LIST, 
+								CV_CHAIN_APPROX_SIMPLE 
+							   );
+				//iterate through found contours					
+				while(contours != NULL)
+				{
+					float aspect_ratio = 0; 
+					if( contours->total >= 6 )
+					{
+						//make cvBox2D from potentialy usable contours, and find actual usable ones by their aspect ratios
+						box_to_check = cvFitEllipse2( contours );
+						if( box_to_check.size.width >= box_to_check.size.height )
+							aspect_ratio = box_to_check.size.width / box_to_check.size.height;
+						else
+							aspect_ratio = box_to_check.size.height / box_to_check.size.width;	
+							
+						if( aspect_ratio >= ASPECT_RATIO_BOUNDARY )
+						{
+						 //sort usable contours by their perimeter, discard those that are too small					
+							double perimeterbound = (gray_input.rows + gray_input.cols)/perimScale; 
+							double totalperimeter = box_to_check.size.width * 2 + box_to_check.size.height * 2;
+							
+							if (totalperimeter >= perimeterbound)
+							{
+								usable_boxes.push_back( box_to_check );
+								cvEllipseBox( 
+											  &IplImage( input ), 
+											  box_to_check, 
+											  CV_RGB( 255, 0, 0 ), 
+											  3, 
+											  8, 
+											  0 
+											 );
+							 } //end perimeter check
+						} //end aspect ratio check
+					} //end contour total check
+					
+					contours = contours->h_next;
+				}
+				cv::imshow( "Found_boxes", input );
+			} //end if orange
 	}
+
+        cvWaitKey( 20 );
+    }
     
     QUICKDEV_SPIN_ONCE()
     {
