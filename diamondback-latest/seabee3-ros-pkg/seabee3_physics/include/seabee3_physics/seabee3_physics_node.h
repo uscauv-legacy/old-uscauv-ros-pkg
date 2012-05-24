01951a62bfc8ee3ab863804dcd013530f1530cff
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-05-24 05:11:25 +0000

    Removed angular damping; added per-axis linear damping. Orientation will be set directly from IMU.

--- diamondback-latest/seabee3-ros-pkg/seabee3_physics/include/seabee3_physics/seabee3_physics_node.h
+++ diamondback-latest/seabee3-ros-pkg/seabee3_physics/include/seabee3_physics/seabee3_physics_node.h
@@ -38,21 +38,25 @@
 
 #include <quickdev/node.h>
 
+// policies
 #include <quickdev/reconfigure_policy.h>
 #include <quickdev/tf_tranceiver_policy.h>
 
+// objects
 #include <quickdev/time.h>
 #include <quickdev/multi_publisher.h>
 #include <quickdev/multi_subscriber.h>
 #include <quickdev/threading.h>
 
+// utils
 #include <seabee3_common/movement.h>
-
 #include "btBulletDynamicsCommon.h"
 
+// msgs
 #include <seabee3_driver/MotorVals.h>
 #include <seabee3_driver/KillSwitch.h>
 
+// cfgs
 #include <seabee3_physics/Seabee3PhysicsConfig.h>
 
 using namespace seabee3_common;
@@ -76,9 +80,13 @@ QUICKDEV_DECLARE_NODE_CLASS( Seabee3Physics )
     boost::shared_ptr<btCollisionDispatcher> dispatcher_;
     boost::shared_ptr<btSequentialImpulseConstraintSolver> solver_;
     boost::shared_ptr<btDiscreteDynamicsWorld> dynamics_world_;
-    boost::shared_ptr<btCollisionShape> seabee_shape_;
     boost::shared_ptr<btDefaultMotionState> seabee_motion_state_;
     boost::shared_ptr<btRigidBody> seabee_body_;
+    boost::shared_ptr<btCollisionShape> seabee_shape_;
+
+    std::map<std::string, boost::shared_ptr<btCollisionShape> > structure_dynamics_map_;
+    std::map<std::string, boost::shared_ptr<btCollisionShape> > structures_map_;
+    std::map<std::string, btTransform> transforms_map_;
 
     quickdev::Timer timer_;
 
@@ -91,7 +99,84 @@ QUICKDEV_DECLARE_NODE_CLASS( Seabee3Physics )
         thruster_values_( movement::NUM_THRUSTERS ),
         is_killed_( true )
     {
-        // Build the broadphase
+        //
+    }
+
+    boost::shared_ptr<btCollisionShape> makeBtCollisionShape( XmlRpc::XmlRpcValue && value )
+    {
+        boost::shared_ptr<btCollisionShape> result;
+
+        if( value["type"] == "box" )
+        {
+            double const length = value["length"];
+            double const width = value["width"];
+            double const height = value["height"];
+            result = boost::shared_ptr<btCollisionShape>( new btBoxShape( btVector3( length / 2, width / 2, height / 2 ) ) );
+        }
+
+        if( value["type"] == "cylinder" )
+        {
+            double const length = value["length"];
+            double const width = value.hasMember( "width" ) ? double( value["width"] ) : 2 * double( value["radius"] );
+            double const height = value.hasMember( "height" ) ? double( value["height"] ) : 2 * double( value["radius"] );
+
+            result = boost::shared_ptr<btCollisionShape>( new btCylinderShapeX( btVector3( length / 2, width / 2, height / 2 ) ) );
+        }
+
+        // if( result ) result->calculateLocalInertia( double( value["mass"] ), btVector3( 0, 0, 0 ) );
+
+        return result;
+    }
+
+    QUICKDEV_DECLARE_RECONFIGURE_CALLBACK( reconfigureCB, _Seabee3PhysicsConfig )
+    {
+        // if( seabee_body_ ) seabee_body_->setDamping( config.linear_damping, config.angular_damping );
+    }
+
+    QUICKDEV_SPIN_FIRST()
+    {
+        QUICKDEV_GET_RUNABLE_NODEHANDLE( nh_rel );
+
+        multi_sub_.addSubscriber( nh_rel, "motor_vals", &Seabee3PhysicsNode::motorValsCB, this );
+        multi_sub_.addSubscriber( nh_rel, "kill_switch", &Seabee3PhysicsNode::killSwitchCB, this );
+
+        // read in structures from parameter server
+        auto structures_param = quickdev::ParamReader::readParam<XmlRpc::XmlRpcValue>( nh_rel, "structures" );
+        // read in structure dynamics from parameter server
+        auto structure_dynamics_param = quickdev::ParamReader::readParam<XmlRpc::XmlRpcValue>( nh_rel, "structure_dynamics" );
+
+        // build map of collision shapes from structure dynamics parameter
+        for( auto structure_dynamics_it = structure_dynamics_param.begin(); structure_dynamics_it != structure_dynamics_param.end(); ++structure_dynamics_it )
+        {
+            structure_dynamics_map_[structure_dynamics_it->first] = makeBtCollisionShape( structure_dynamics_it->second );
+        }
+
+        // build map of named collision shapes and corresponding transforms from structures parameter
+        for( auto structures_it = structures_param.begin(); structures_it != structures_param.end(); ++structures_it )
+        {
+            // look up the dynamics entry for this structure
+            auto structure_dynamics_it = structure_dynamics_map_.find( structures_it->second["dynamics"] );
+            // if it exists (ie there is porn of it in the map)
+            if( structure_dynamics_it != structure_dynamics_map_.end() )
+            {
+                structures_map_[structures_it->first] = structure_dynamics_it->second;
+
+                btTransform current_transform = btTransform( btQuaternion( 0, 0, 0, 1 ), btVector3( 0, 0, 0 ) );
+
+                // attempt to look up the transform from the parent of this structure to this structure
+                if( structures_it->second.hasMember( "parent" ) )
+                {
+                    std::string const parent_name = structures_it->second["parent"];
+                    std::string const from_frame = structures_param[parent_name]["frame"];
+                    std::string const to_frame = structures_it->second["frame"];
+                    current_transform = _TfTranceiverPolicy::tryLookupTransform( from_frame, to_frame );
+                }
+
+                transforms_map_[structures_it->first] = current_transform;
+            }
+        }
+
+        // Build the broadphase; this object helps determine which objects are about to collide
         broadphase_ = quickdev::make_shared( new btDbvtBroadphase() );
 
         // Set up the collision configuration and dispatcher
@@ -103,36 +188,35 @@ QUICKDEV_DECLARE_NODE_CLASS( Seabee3Physics )
 
         // The world
         dynamics_world_ = quickdev::make_shared( new btDiscreteDynamicsWorld( dispatcher_.get(), broadphase_.get(), solver_.get(), collision_configuration_.get() ) );
+        // zero grav
         dynamics_world_->setGravity( btVector3( 0, 0, 0 ) );
 
         // Seabee's Body
-        seabee_shape_ = quickdev::make_shared( new btCylinderShape( btVector3( .5, .2, .2 ) ) );
+        /*seabee_shape_ = boost::make_shared<btCompoundShape>();
+        for( auto structures_it = structures_map_.begin(); structures_it != structures_map_.end(); ++structures_it )
+        {
+            seabee_shape_->addChildShape( transforms_map_[structures_it->first], structures_it->second.get() );
+        }*/
+        seabee_shape_ = structures_map_["hull"];
 
         seabee_motion_state_ = quickdev::make_shared( new btDefaultMotionState( btTransform( btQuaternion( 0, 0, 0, 1 ), btVector3( 0, 0, 0 ) ) ) );
 
-        // She weighs 35kg
-        btScalar seabee_mass = 35;
+        btScalar seabee_mass = 27.6010957;
         btVector3 seabee_inertia( 0, 0, 0 );
         seabee_shape_->calculateLocalInertia( seabee_mass, seabee_inertia );
 
         btRigidBody::btRigidBodyConstructionInfo seabee_body_ci( seabee_mass, seabee_motion_state_.get(), seabee_shape_.get(), seabee_inertia );
         seabee_body_ = quickdev::make_shared( new btRigidBody( seabee_body_ci ) );
         seabee_body_->setActivationState( DISABLE_DEACTIVATION );
-        seabee_body_->setDamping( 0.018, 0 );
+        // seabee_body_->setDamping( config_.linear_damping, config_.angular_damping );
 
         dynamics_world_->addRigidBody( seabee_body_.get() );
-    }
-
-    QUICKDEV_SPIN_FIRST()
-    {
-        QUICKDEV_GET_RUNABLE_NODEHANDLE( nh_rel );
-
-        multi_sub_.addSubscriber( nh_rel, "motor_vals", &Seabee3PhysicsNode::motorValsCB, this );
-        multi_sub_.addSubscriber( nh_rel, "kill_switch", &Seabee3PhysicsNode::killSwitchCB, this );
 
         //multi_pub_.addPublishers<
 
         timer_.reset();
+
+        _Seabee3PhysicsLiveParams::registerCallback( quickdev::auto_bind( &Seabee3PhysicsNode::reconfigureCB, this ) );
 
         initPolicies<quickdev::policy::ALL>();
 
@@ -199,65 +283,32 @@ QUICKDEV_DECLARE_NODE_CLASS( Seabee3Physics )
             seabee_body_->applyForce( rotated_force, force_rel_pos );
         }
 
-        btVector3 const & lin_vel = seabee_body_->getLinearVelocity();
-        //btVector3 force_drag = -lin_v_ * reconfigure_params_.drag_constant;
+        btVector3 const & linear_velocity = seabee_body_->getLinearVelocity();
+        btVector3 drag_force( -config_.drag_x * linear_velocity.x(), -config_.drag_y * linear_velocity.y(), -config_.drag_z * linear_velocity.z() );
 
-        /* btVector3 force_drag;
+        seabee_body_->applyForce( drag_force, seabee_body_->getCenterOfMassPosition() );
 
-        //! Todo: The way these constants were calculated needs to be clearer (ie. they need to be functions of measurements of the sub)
-        force_drag.setX( -0.5 * 1000 * 0.66 * 0.35 * lin_vel.x() * lin_vel.x() * 0.81 );
-        force_drag.setY( -0.5 * 1000 * 0.66 * 0.35 * lin_vel.y() * lin_vel.y() * 0.42 );
-        force_drag.setZ( -0.5 * 1000 * 0.66 * 0.66  * lin_vel.z() * lin_vel.z() * 0.42 );
+        ROS_INFO( "Drag Force: %f ... Sub Speed: %f", drag_force.length(), linear_velocity.length() );
 
-        seabee_body_->applyForce( force_drag, seabee_body_->getCenterOfMassPosition() );
-
-        ROS_INFO( "Drag Force: %f ... Sub Speed: %f", force_drag.length(), lin_vel.length() );*/
-
-        ROS_INFO( "lin_vel: %f, %f, %f", lin_vel.x(), lin_vel.y(), lin_vel.z() );
-
-        /*btVector3 torque;
-
-        double _torque[3] = {0,0,0};
-
-        for ( int i = 1; i < 2; i++ ) {
-
-           _torque[0] += force_drag.getY() * force_drag.getZ() * 0.66 / i;
-           _torque[1] += force_drag.getX() * force_drag.getZ() * 0.66 / i;
-           _torque[2] += force_drag.getX() * force_drag.getY() * 0.35 / i;
-
-           }
-
-        torque.setX( -1*_torque[0] );
-        torque.setY( -1*_torque[1] );
-        torque.setZ( -1*_torque[2] );
-
-        seabee_body_->applyTorque( torque );
-
-        ROS_INFO( "Torque: %f", torque.length() );*/
+        ROS_INFO( "linear_velocity: %f, %f, %f", linear_velocity.x(), linear_velocity.y(), linear_velocity.z() );
 
         auto const & dt = timer_.update();
 
-        btScalar ang_damping = 0.3;
-        btScalar lin_damping = 0.3;
-
-        seabee_body_->setDamping ( lin_damping, ang_damping );
-
-        seabee_body_->applyDamping(dt);
+        //seabee_body_->applyDamping( dt );
 
         // Step the physics simulation; increase the simulation's time by dt (dt is variable); interpolate by up to 2 steps when dt is not the ideal @loop_rate_seconds_ time
         dynamics_world_->stepSimulation( dt, 2, getLoopRateSeconds() );
 
         //tf::Transform givens_tf;
-        //tf_utils::fetchTfFrame( givens_tf, "/landmark_map", "/seabee3/base_link_givens" );
+        auto const imu_transform = _TfTranceiverPolicy::tryLookupTransform( "/world", "/seabee3/imu" );
+        auto const depth_transform = _TfTranceiverPolicy::tryLookupTransform( "/world", "/seabee3/depth" );
 //      geometry_msgs::Twist givens;
 //      givens_tf >> givens;
 
-      //btTransform center_of_mass;
-//
-        //auto center_of_mass = seabee_body_->getCenterOfMassTransform();
-        //center_of_mass.setRotation( givens_tf.getRotation() );
-        //center_of_mass.getOrigin().setZ( givens_tf.getOrigin().getZ() );
-        //seabee_body_->setCenterOfMassTransform( center_of_mass );
+        auto center_of_mass = seabee_body_->getCenterOfMassTransform();
+        center_of_mass.setRotation( imu_transform.getRotation() );
+        center_of_mass.getOrigin().setZ( depth_transform.getOrigin().getZ() );
+        seabee_body_->setCenterOfMassTransform( center_of_mass );
 
 /*        seabee_body_->getMotionState()->getWorldTransform( world_transform );
         world_transform.setRotation( givens_tf.getRotation() );
@@ -278,7 +329,7 @@ QUICKDEV_DECLARE_NODE_CLASS( Seabee3Physics )
                 world_transform.getOrigin().getY(),
                 world_transform.getOrigin().getZ() );
 
-        publishTransform( world_transform, "/world", "/seabee3/physics_link" );
+        _TfTranceiverPolicy::publishTransform( world_transform, "/world", "/seabee3/physics_link" );
     }
 
     QUICKDEV_DECLARE_MESSAGE_CALLBACK( motorValsCB, _MotorValsMsg )
