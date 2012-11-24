cf9499dc8d392c1f8d6899e82f79480d3f3a4ce8
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-11-24 09:52:44 -0800

    Cleaned up code a bit; removed unnecessary non-unit-vector drag calculations.

--- seabee3_physics/ang_drag_test.m
+++ seabee3_physics/ang_drag_test.m
@@ -7,7 +7,7 @@ sample_pts_u = zeros( n, 2 )
 sample_pts_v = zeros( n, 2 )
 sample_vecs_u = zeros( n, 2 )
 sample_vecs_v = zeros( n, 2 )
-drag_vecs = zeros( 2 * n, 2 )
+drag_unit_vecs = zeros( 2 * n, 2 )
 
 # boundary points for the rectangle
 dim_max = max( dim_x, dim_y ) / 2 + 1
@@ -22,10 +22,11 @@ for i = 1:n
 	sample_pts_v(i,:) = [ (2*i-1)*dim_x/( 4 * n ), dim_y / 2 ]
 end
 
+# combine u/v points
 sample_pts = [ sample_pts_u; sample_pts_v ]
 
 for i = 1:rows( sample_pts_u )
-	# generate drag vectors for vertical and horizontal faces
+	# generate sample vectors for vertical and horizontal faces
 	sample_vec_u = sample_pts_u(i,:) * [0 1; -1 0 ]
 	sample_vec_v = sample_pts_v(i,:) * [0 1; -1 0 ]
 
@@ -33,9 +34,10 @@ for i = 1:rows( sample_pts_u )
 	sample_vecs_u(i,:) = sample_vec_u / norm( sample_vec_u )
 	sample_vecs_v(i,:) = sample_vec_v / norm( sample_vec_v )
 	
-	# the drag vector along the y axis 
-	drag_vecs(i,:) = [ -sample_vecs_u(i,1), 0 ]
-	drag_vecs(i+rows(sample_pts_u),:) = [ 0, -sample_vecs_v(i,2) ]
+	# the unit drag vector along the x axis 
+	drag_unit_vecs(i,:) = [ -sample_vecs_u(i,1), 0 ]
+	# the unit drag vector along the y axis
+	drag_unit_vecs(i+rows(sample_pts_u),:) = [ 0, -sample_vecs_v(i,2) ]
 end
 
 # combine u/v vecs
@@ -44,14 +46,14 @@ sample_vecs = [ sample_vecs_u; sample_vecs_v ]
 # mirror values to other corners (origin symmetry)
 sample_vecs = [ sample_vecs; -sample_vecs ]
 sample_pts = [ sample_pts; -sample_pts ]
-drag_vecs = [drag_vecs; -drag_vecs ]
+drag_unit_vecs = [drag_unit_vecs; -drag_unit_vecs ]
 
 # transform from 3D x/y plane to 2D x/y plane
-sample_vecs = sample_vecs * [ 0 1; -1 0 ]
-sample_pts = sample_pts * [ 0 1; -1 0 ]
-drag_vecs = drag_vecs * [ 0 1; -1 0 ]
-bounds_graph = bounds_graph * [ 0 1; -1 0 ]
-bounds_points = bounds_points * [ 0 1; -1 0 ]
+sample_vecs *= [ 0 1; -1 0 ]
+sample_pts *= [ 0 1; -1 0 ]
+drag_unit_vecs *= [ 0 1; -1 0 ]
+bounds_graph *= [ 0 1; -1 0 ]
+bounds_points *= [ 0 1; -1 0 ]
 
 # draw
 clf
@@ -62,5 +64,5 @@ plot( sample_pts(:,1), sample_pts(:,2), "@1" )
 
 for i = 1:rows( sample_vecs )
 	quiver( sample_pts(i,1), sample_pts(i,2), sample_vecs(i,1), sample_vecs(i,2), "2" )
-	quiver( sample_pts(i,1), sample_pts(i,2), drag_vecs(i,1), drag_vecs(i,2), "1" )
+	quiver( sample_pts(i,1), sample_pts(i,2), drag_unit_vecs(i,1), drag_unit_vecs(i,2), "1" )
 end
