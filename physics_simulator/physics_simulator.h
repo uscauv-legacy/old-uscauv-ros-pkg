f2f7a85e008a5ac3c6c7add90d376346f34c0266
Francesca Nannizzi <fnannizzi@gmail.com>
2013-02-10 00:37:48 -0800

    This is so embarrassing

---
+++ physics_simulator/physics_simulator.h
@@ -0,0 +1,36 @@
+/* New physics simulator			|
+____________________________________|*/
+
+
+
+#include <ros/ros.h>
+#include <std_msgs/String.h> //what to use for msgs?
+
+using namespace seabee3_common;
+
+class physics_simulator {
+	public:o
+		physics_simulator(); // Constructor, what params?
+		~physics_simulator(){}; // Destructor
+		initialize_simulator();
+		
+	private:
+		//callback function?
+		
+
+	void physics_simulator()
+	{
+		ros::init(//argc, argv, "name"?
+		 );
+		 
+		 
+
+	}
+
+	void initialize_simulator()
+	{
+
+	}
+	
+	
+}
