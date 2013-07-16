9c0b9b4ab748287c4eb91895fdb91d33c5ac4adf
Dylan Foster <turtlecannon@gmail.com>
2013-07-15 22:22:31 -0700

    Added pose input to control_server.

--- auv_controls/include/auv_controls/control_server_node.h
+++ auv_controls/include/auv_controls/control_server_node.h
@@ -53,6 +53,9 @@
 
 #include <eigen_conversions/eigen_msg.h>
 
+static std::string const DESIRED_FRAME_NAME = "robot/controls/desired";
+static std::string const MEASUREMENT_FRAME_NAME = "robot/controls/measurement";
+
 class ControlServerNode: public BaseNode, public uscauv::PID6D
 {
  public:
@@ -62,47 +65,47 @@ class ControlServerNode: public BaseNode, public uscauv::PID6D
   typedef auv_msgs::MaskedTwist _MaskedTwistMsg;
   
  private:
-  /// 
+
   uscauv::ReconfigurableThrusterAxisModel thruster_axis_model_;
   
-  AxisValueVector axis_command_value_;
+  AxisValueVector axis_command_value_, pose_command_value_;
   AxisMaskVector axis_command_mask_;
 
   /// ROS
   ros::NodeHandle nh_rel_;
   ros::Subscriber axis_command_sub_;
   ros::Publisher motor_pub_;
+  tf::TransformListener tf_listener_;
   
  public:
  ControlServerNode(): BaseNode("ControlServer"), thruster_axis_model_("model/thrusters"), 
-    axis_command_value_( AxisValueVector::Zero()), axis_command_mask_( AxisMaskVector::Zero() ),
-    nh_rel_("~")
-   {
-   }
+    axis_command_value_( AxisValueVector::Zero() ), pose_command_value_( AxisValueVector::Zero() ),
+    axis_command_mask_( AxisMaskVector::Zero() ),
+    nh_rel_("~") {}
 
  private:
 
   // Running spin() will cause this function to be called before the node begins looping the spinOnce() function.
   void spinFirst()
-     {
-       axis_command_sub_ = nh_rel_.subscribe( "axis_cmd", 10, &ControlServerNode::axisCommandCallback, this );
+  {
+    axis_command_sub_ = nh_rel_.subscribe( "axis_cmd", 10, &ControlServerNode::axisCommandCallback, this );
 
-       motor_pub_ = nh_rel_.advertise<auv_msgs::MotorPowerArray>( "motor_levels", 10 );
+    motor_pub_ = nh_rel_.advertise<auv_msgs::MotorPowerArray>( "motor_levels", 10 );
        
-       /// Load thruster models
-       thruster_axis_model_.load("robot/thrusters");
+    /// Load thruster models
+    thruster_axis_model_.load("robot/thrusters");
 
-       /// Load PIDs
-       loadController();
+    /// Load PIDs
+    loadController();
 
-       setObserved<PID6D::Axes::SURGE> (0.0f);
-       setObserved<PID6D::Axes::SWAY>  (0.0f);
-       setObserved<PID6D::Axes::HEAVE> (0.0f);
-       setObserved<PID6D::Axes::ROLL>  (0.0f);
-       setObserved<PID6D::Axes::PITCH> (0.0f);
-       setObserved<PID6D::Axes::YAW>   (0.0f);
+    setObserved<PID6D::Axes::SURGE> (0.0f);
+    setObserved<PID6D::Axes::SWAY>  (0.0f);
+    setObserved<PID6D::Axes::HEAVE> (0.0f);
+    setObserved<PID6D::Axes::ROLL>  (0.0f);
+    setObserved<PID6D::Axes::PITCH> (0.0f);
+    setObserved<PID6D::Axes::YAW>   (0.0f);
        
-     }  
+  }  
 
   // Running spin() will cause this function to get called at the loop rate until this node is killed.
   void spinOnce()
@@ -141,20 +144,76 @@ class ControlServerNode: public BaseNode, public uscauv::PID6D
 
   void updateSetpoints()
   {
-    if( axis_command_mask_(0) )
-      setSetpoint<PID6D::Axes::SURGE>( axis_command_value_(0) );
-    if( axis_command_mask_(1) )
-      setSetpoint<PID6D::Axes::SWAY>( axis_command_value_(1) );
-    if( axis_command_mask_(2) )
-      setSetpoint<PID6D::Axes::HEAVE>( axis_command_value_(2) );
-    if( axis_command_mask_(3) )
-      setSetpoint<PID6D::Axes::ROLL>( axis_command_value_(3) );
-    if( axis_command_mask_(4) )
-      setSetpoint<PID6D::Axes::PITCH>( axis_command_value_(4) );
-    if( axis_command_mask_(5) )
-      setSetpoint<PID6D::Axes::YAW>( axis_command_value_(5) );
+    AxisValueVector setpoint = pose_command_value_ + axis_command_value_;
+    
+    setSetpoint<PID6D::Axes::SURGE>( setpoint(0) );
+    setSetpoint<PID6D::Axes::SWAY> ( setpoint(1) );
+    setSetpoint<PID6D::Axes::HEAVE>( setpoint(2) );
+    setSetpoint<PID6D::Axes::ROLL> ( setpoint(3) );
+    setSetpoint<PID6D::Axes::PITCH>( setpoint(4) );
+    setSetpoint<PID6D::Axes::YAW>  ( setpoint(5) );
+
+    /* if( axis_command_mask_(0) ) */
+    /*   setSetpoint<PID6D::Axes::SURGE>( axis_command_value_(0) ); */
+    /* if( axis_command_mask_(1) ) */
+    /*   setSetpoint<PID6D::Axes::SWAY>( axis_command_value_(1) ); */
+    /* if( axis_command_mask_(2) ) */
+    /*   setSetpoint<PID6D::Axes::HEAVE>( axis_command_value_(2) ); */
+    /* if( axis_command_mask_(3) ) */
+    /*   setSetpoint<PID6D::Axes::ROLL>( axis_command_value_(3) ); */
+    /* if( axis_command_mask_(4) ) */
+    /*   setSetpoint<PID6D::Axes::PITCH>( axis_command_value_(4) ); */
+    /* if( axis_command_mask_(5) ) */
+    /*   setSetpoint<PID6D::Axes::YAW>( axis_command_value_(5) ); */
   }
 
+  void updatePoseCommand()
+  {
+    tf::StampedTransform world_to_desired_tf, world_to_measurement_tf;
+    
+    if( tf_listener_.canTransform( "/world", MEASUREMENT_FRAME_NAME, ros::Time(0) ))
+      {
+	try
+	  {
+	    tf_listener_.lookupTransform( "/world", MEASUREMENT_FRAME_NAME, ros::Time(0), world_to_measurement_tf);
+
+	  }
+	catch(tf::TransformException & ex)
+	  {
+	    ROS_ERROR( "%s", ex.what() );
+	    return;
+	  }
+      }
+    else return;
+    
+    if( tf_listener_.canTransform( "/world", DESIRED_FRAME_NAME, ros::Time(0) ))
+      {
+	try
+	  {
+	    tf_listener_.lookupTransform( "/world", DESIRED_FRAME_NAME, ros::Time(0), world_to_desired_tf);
+
+	  }
+	catch(tf::TransformException & ex)
+	  {
+	    ROS_ERROR( "%s", ex.what() );
+	    return;
+	  }
+      }
+    else return;
+
+    tf::Transform error_tf = world_to_measurement_tf.inverse() * world_to_desired_tf;
+    double roll, pitch, yaw;
+    
+    error_tf.getBasis().getRPY( roll, pitch, yaw );
+
+    tf::Vector3 error_vec = error_tf.getOrigin();
+
+    AxisValueVector error_pose_value;
+    error_pose_value << error_vec.x(), error_vec.y(), error_vec.z(), roll, pitch, yaw;
+    
+    pose_command_value_ = error_pose_value;
+  }
+  
 };
 
 #endif // USCAUV_AUVCONTROLS_CONTROLSERVER
